063eb8fa532e37df148757bcf9479c55be97e25b
jim.carreer <jim.carreer@0a1ef842-052b-11de-b753-7319d013a8fa>
2009-03-03 18:32:59 +0000

    simout.c simout.h added major changes to main and thread arguments, also implementing use of semaphores for thread control.

--- trunk/main.c
+++ trunk/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <semaphore.h>
 #include <pthread.h>
 #include <math.h>
 #include "customer.h"
@@ -16,44 +17,90 @@
 #define DEFAULT_QMODE     FIFO
 #define DEFAULT_SEED      0
 
-//Simulation queues
-cqueue* live; //Queue of waiting customers
-cqueue* dead; //Queue of served customers for statistics
-
-//Flags and locks
-pthread_mutex_t lock;         //Thread locking mutex
-int terminate_simulation = 0; //Kill flag to kill the simulation
-int genisis_complete     = 0; //Flag to signal no more customers being created
-
-//Structures and typedefs
+//Thread input structures
 typedef struct _genisis_data {
-    int    customers;
-    double lambda;
-    double mu;
-    double rseed;
+    int              customers;       //Total number of customers to generate
+    double           lambda;          //Arrival time exponentional distribution parameter
+    double           mu;              //Service time exponentional distribution parameter
+    double           rseed;           //Seed for random numbers
+    cqueue*          live;            //Referance to queue for live (unserviced) customers
+    pthread_mutex_t* livelock;        //Referance to mutex to lock live queue
+    pthread_mutex_t* dmemlock;        //Referance to mutex to lock for memory allocation
+    sem_t*           terminate;       //Referance to terminate simulation flag
+    sem_t*           customers_left;  //Referance to number of customers left to generate
 } genisis_data;
+
+typedef struct _statistics_data {
+    int              customers;       //Total number of customers being generated
+    int              servers;         //Total number of servers
+    double           lambda;          //Arrival time exponentional distribution parameter
+    double           mu;              //Service time exponentional distribution parameter
+    cqueue*          live;            //Referance to queue for live (unserviced) customers
+    cqueue*          dead;            //Referance to queue for dead (serviced) customers
+    pthread_mutex_t* deadlock;        //Referance to mutex to lock dead queue
+    pthread_mutex_t* livelock;        //Referance to mutex to lock live queue
+    pthread_mutex_t* dmemlock;        //Referance to mutex to lock for memory allocation
+    sem_t*           terminate;       //Referance to terminate simulation flag
+    sem_t*           customers_left;  //Referance to number of customers left to generate
+} statistics_data;
+
+typedef struct _service_data {
+    int              stid;            //Service thread number
+    cqueue*          live;            //Referance to queue for live (unserviced) customers
+    cqueue*          dead;            //Referance to queue for dead (serviced) customers
+    pthread_mutex_t* deadlock;        //Referance to mutex to lock dead queue
+    pthread_mutex_t* livelock;        //Referance to mutex to lock live queue
+    sem_t*           terminate;       //Referance to terminate simulation flag
+    sem_t*           customers_left;  //Referance to number of customers left to generate
+} service_data;
 
 //Prototypes and inline functions
 inline double rexp(double l) {return -log(1.0-drand48())/l;}
 void* genisis(void*);
 void* service(void*);
-void* output(void);
+void* statistics(void*);
 void* watchman(void);
 void  psleep(double interval);
 
 int main(int argc, char** argv)
 {
-    genisis_data gdata;
+    ////////////////////////////////////
+    //Queue variables
+    cqueue*          live;
+    cqueue*          dead;
+    ////////////////////////////////////
+    //Thread parameters
+    genisis_data     gensd;
+    statistics_data  statd;
+    service_data*    servd;
+    ////////////////////////////////////
+    //Thread related variables
+    pthread_mutex_t  deadlock;
+    pthread_mutex_t  livelock;
+    pthread_mutex_t  dmemlock;
+
+    sem_t            terminate;
+    sem_t            customers_left;
+
+    pthread_t*       service_t;
+    pthread_t        genisis_t;
+    pthread_t        statistics_t;
+    pthread_t        watchman_t;
+
+    pthread_attr_t   attributes;
+
+    int              status, terror, i;
+    ////////////////////////////////////
+    //Simulation related variables
     cqmode mode      = DEFAULT_QMODE;
     int    servers   = DEFAULT_SERVERS;
-    gdata.rseed      = DEFAULT_SEED;
-    gdata.customers  = DEFAULT_CUSTOMERS;
-    gdata.lambda     = DEFAULT_LAMBDA;
-    gdata.mu         = DEFAULT_MU;
+    double rseed     = DEFAULT_SEED;
+    int    customers = DEFAULT_CUSTOMERS;
+    double lambda    = DEFAULT_LAMBDA;
+    double mu        = DEFAULT_MU;
 
     ////////////////////////////////////////////////////////////////////////
     // Parse command line parameters
-    int i = 0;
     for(i = 1; i < argc; i++) {
         switch(argv[i][1]) {
             case 'N':
@@ -68,28 +115,28 @@ int main(int argc, char** argv)
                     printf("Incomplete argument 'L'\n");
                     exit(-1);
                 }
-                gdata.lambda = (double)atof(argv[++i]);
+                lambda = (double)atof(argv[++i]);
                 break;
             case 'M':
                 if(i+1 >= argc) {
                     printf("Incomplete argument 'M'\n");
                     exit(-1);
                 }
-                gdata.mu = (double)atof(argv[++i]);
+                mu = (double)atof(argv[++i]);
                 break;
             case 'T':
                 if(i+1 >= argc) {
                     printf("Incomplete argument 'T'\n");
                     exit(-1);
                 }
-                gdata.customers = atoi(argv[++i]);
+                customers = atoi(argv[++i]);
                 break;
             case 'R':
                 if(i+1 >= argc) {
                     printf("Incomplete argument 'R'\n");
                     exit(-1);
                 }
-                gdata.rseed = (double)atof(argv[++i]);
+                rseed = (double)atof(argv[++i]);
                 break;
             case 'S':
                 if(i+1 >= argc) {
@@ -113,25 +160,11 @@ int main(int argc, char** argv)
     if(servers > SERVER_MAX || servers <= 0) {
         printf("The number of servers is restricted between 1 and %d\n",SERVER_MAX);
         exit(-1);
-    } else if(gdata.mu*servers < gdata.lambda) {
+    } else if(mu*servers < lambda) {
         printf("The product of mu and the number of servers must be greater than lambda\n");
         exit(-1);
     }
-    if(!gdata.rseed) gdata.rseed = time(NULL);
-
-    ////////////////////////////////////////////////////////////////////////
-    //Configuration debug statements
-    #ifdef DEBUG
-    printf("Servers\t\t: %d\n",servers);
-    printf("Mu\t\t: %lf\n",gdata.mu);
-    printf("Lambda\t\t: %lf\n",gdata.lambda);
-    printf("Seed\t\t: %lf\n",gdata.rseed);
-    printf("Iterations\t: %d\n",gdata.customers);
-    if(mode == FIFO)
-        printf("Mode\t\t: FIFO\n");
-    else
-        printf("Mode\t\t: SJF\n");
-    #endif
+    if(!rseed) rseed = time(NULL);
 
     ////////////////////////////////////////////////////////////////////////
     //Setup queues
@@ -145,50 +178,107 @@ int main(int argc, char** argv)
     }
 
     /////////////////////////////////////////////////////////////////////////
-    //Setup threads
-    pthread_t      service_t[SERVER_MAX];
-    pthread_t      genisis_t;
-    pthread_t      output_t;
-    pthread_t      watchman_t;
-    pthread_attr_t attributes;
-    int            status, terror;
+    //Threading
+    //Setup service thread and service thread data
+    service_t = (pthread_t*)malloc(servers*sizeof(pthread_t));
+    servd     = (service_data*)malloc(servers*sizeof(service_data));
+    if(!service_t || !servd) {
+        printf("Error: service thread memory allocation failed\n");
+        destroy_cqueue(live);
+        destroy_cqueue(dead);
+        if(service_t) free(service_t);
+        if(servd) free(servd);
+        exit(-1);
+    }
 
+    //Initialize flags
+    sem_init(terminate, 0, 1);
+    sem_init(customers_left, 0, customers);
+    //Initialize mutexes
+    pthread_mutex_init(&deadlock,NULL);
+    pthread_mutex_init(&livelock,NULL);
+    pthread_mutex_init(&dmemlock,NULL);
+    //Initialize thread attirbutes
     pthread_attr_init(&attributes);
     pthread_attr_setdetachstate(&attributes, PTHREAD_CREATE_JOINABLE);
-    pthread_mutex_init(&lock,NULL);
 
-    if((terror = pthread_create(&genisis_t,&attributes,(void*)genisis,(void*)&gdata))) {
+    //Initialize genisis data
+    gensd.customers_left = &customers_left;
+    gensd.customers      = customers;
+    gensd.lambda         = lambda;
+    gensd.mu             = mu;
+    gensd.live           = live;
+    gensd.livelock       = &livelock;
+    gensd.dmemlock       = &dmemlock;
+    gensd.terminate      = &terminate;
+    //Initialize statistics data
+    statd.customers_left = &customers_left;
+    statd.customers      = customers;
+    statd.servers        = servers;
+    statd.lambda         = lambda;
+    statd.mu             = mu;
+    statd.live           = live;
+    statd.dead           = dead;
+    statd.livelock       = &livelock;
+    statd.deadlock       = &deadlock;
+    statd.dmemlock       = &dmemlock;
+    statd.terminate      = &terminate;
+    //Initialize service data
+    for(i = 0; i < servers; i++) {
+        servd[i].customers_left = &customers_left;
+        servd[i].stid           = i;
+        servd[i].live           = live;
+        servd[i].dead           = dead;
+        servd[i].deadlock       = &deadlock;
+        servd[i].livelock       = &livelock;
+        servd[i].terminate      = &terminate;
+    }
+    /*
+    //Start gensis thread
+    if((terror = pthread_create(&genisis_t,&attributes,(void*)genisis,(void*)&gensd))) {
         printf("Error creating gensis thread (Code:%d)\n",terror);
         destroy_cqueue(live);
         destroy_cqueue(dead);
+        free(service_t);
+        free(servd);
         exit(-1);
     }
+    //Start server threads
     for(i = 0; i < servers; i++) {
-        if((terror = pthread_create(&service_t[i],&attributes,(void*)service,(void*)i))) {
+        if((terror = pthread_create(&service_t[i],&attributes,(void*)service,(void*)(&servd[i])))) {
             printf("Error creating server thread #%d (Code:%d)\n",i,terror);
             destroy_cqueue(live);
             destroy_cqueue(dead);
+            free(service_t);
+            free(servd);
             exit(-1);
         }
     }
+    pthread_attr_destroy(&attributes);
+
+    //Wait for servers to finish
     for(i = 0; i < servers; i++) {
         if((terror = pthread_join(service_t[i], (void **)&status))) {
             printf("Error joing service thread #%d (Code:%d)\n",i,terror);
             exit(-1);
         }
     }
+    //Wait for genisis to finish
     if((terror = pthread_join(genisis_t, (void **)&status))) {
         printf("Error joing genisis thread (Code:%d)\n",terror);
         exit(-1);
-    }
+    }*/
 
     /////////////////////////////////////////////////////////////////////////
-    //Clean up
-    pthread_attr_destroy(&attributes);
-    pthread_mutex_destroy(&lock);
+    //Clean up dynamically allocated memory and mutexes
+    pthread_mutex_destroy(&dmemlock);
+    pthread_mutex_destroy(&livelock);
+    pthread_mutex_destroy(&deadlock);
     destroy_cqueue(live);
     destroy_cqueue(dead);
-    pthread_exit(NULL);
+    free(service_t);
+    free(servd);
+
     return 0;
 }
 
@@ -200,68 +290,24 @@ void psleep(double interval) {
 }
 
 void* genisis(void* targ) {
-    genisis_data* gdata = (genisis_data*)targ;
+    genisis_data* gensd = (genisis_data*)targ;
     int generated = 0;
     customer* c = NULL;
     timeval birthday;
-    srand48(gdata->rseed);
-    while(generated < gdata->customers) {
-        #ifdef DEBUG
-        printf("Generating(%d)...\n",generated);
-        #endif
-        gettimeofday(&birthday,NULL);
-        pthread_mutex_lock(&lock);
-        if(terminate_simulation) {
-            pthread_mutex_unlock(&lock);
-            pthread_exit(NULL);
-        }
-        c = new_customer(rexp(gdata->mu),birthday);
-        encqueue(live,c);
-        pthread_mutex_unlock(&lock);
-        generated++;
-        psleep(rexp(gdata->lambda));
-    }
-    pthread_mutex_lock(&lock);
-    genisis_complete = 1;
-    pthread_mutex_unlock(&lock);
-    pthread_exit(NULL);
+    srand48(gensd->rseed);
     return NULL;
 }
 
 void* service(void* targ) {
-    int stid = (int)targ;
+    service_data* servd = (service_data*)targ;
     customer* c = NULL;
     timeval deathday;
-    while(1) {
-        pthread_mutex_lock(&lock);
-        if(terminate_simulation) {
-            pthread_mutex_unlock(&lock);
-            pthread_exit(NULL);
-        }
-        if(live->count == 0 && genisis_complete) {
-            pthread_mutex_unlock(&lock);
-            pthread_exit(NULL);
-        }
-        c = decqueue(live);
-        pthread_mutex_unlock(&lock);
 
-        if(c == NULL) {
-            #ifdef DEBUG
-            printf("Server #%d idling...\n",stid);
-            #endif
-            psleep(0.01);
-            continue;
-        }
+    return NULL;
+}
 
-        #ifdef DEBUG
-        printf("Server #%d servicing...\n",stid);
-        #endif
-        psleep(c->job);
-        gettimeofday(&deathday,NULL);
-        c->died = deathday;
-        pthread_mutex_lock(&lock);
-        encqueue(dead, c);
-        pthread_mutex_unlock(&lock);
-    }
+void* statistics(void* targ) {
+    statistics_data* statd = (statistics_data*)targ;
+
     return NULL;
 }
