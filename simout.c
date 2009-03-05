977a43c63c868922689f91e71b995f5501204f7d
jim.carreer <jim.carreer@0a1ef842-052b-11de-b753-7319d013a8fa>
2009-03-05 06:25:02 +0000

    Just fucking around with the screen.

--- simout.c
+++ simout.c
@@ -9,6 +9,8 @@ void screen_init(void) {
     wrefresh(mainwin);
     screen = newwin(25, 50, 1, 1);
     box(screen, ACS_VLINE, ACS_HLINE);
+    curs_set(0);
+    mvwprintw(screen,0,1"Queue Simulation Statistics");
 }
 
 void screen_end(void) {
@@ -18,9 +20,9 @@ void screen_end(void) {
 void update_server(int stid, double utilized, int served) {
     curs_set(0);
     int y = 0 + stid + (3*stid);
-    mvwprintw(screen,y+0,6,"Server #%d Statistics",stid+1);
-    mvwprintw(screen,y+1,6,"Served   : %d",served);
-    mvwprintw(screen,y+2,6,"Utilized : %3.2lf",utilized);
+    mvwprintw(screen,y+0,2,"Server #%d Statistics",stid+1);
+    mvwprintw(screen,y+1,2,"Served   : %d",served);
+    mvwprintw(screen,y+2,2,"Utilized : %3.2lf",utilized);
     wrefresh(screen);
     refresh();
 }
